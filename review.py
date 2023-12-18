4ecebb6589e4d554a20352f38ee83b0565607152
JamesLawwd <jleo56291@gmail.com>
2023-12-19 00:00:02 +0300

    initial commit

--- review.py
+++ review.py
@@ -4,3 +4,10 @@ class Review:
     reviews = []
 
     def __init__(self,customer, restaurant, rating):
+        self._customer = customer
+        self._restaurant = restaurant
+        self._rating = rating 
+
+    @property
+    def rating(self):
+        return self._rating
