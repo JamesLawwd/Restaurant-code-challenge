dbca144929190bf7501d4e1504e0cb282b383a99
JamesLawwd <jleo56291@gmail.com>
2023-12-19 12:04:04 +0300

    test

--- testcase.py
+++ testcase.py
@@ -1,4 +1,27 @@
+import ipdb
 from review import Review
 from restaurant import Restaurant
 from customer import Customer
 
+# new instances of customer
+kinyanjui = Customer("kinyanjui", "James")
+wangeshi = Customer("wangeshi", "winnie")
+wangari = Customer("wangari", "sheenel")
+
+Customer.all   # kinyanjui.find_by_name
+
+restaurant1 = Restaurant("5 star")
+restaurant2 = Restaurant("Everywhere")
+review1 = Review(kinyanjui,restaurant1,3)
+
+# print the number of reviews for each customer
+
+print(review1.all_reviews)
+
+# customer review
+
+review2 = Review(wangeshi,restaurant2, 2)
+
+
+for review in Review.all():
+    print(f"Review Rating: {review.rating}, Customer Data: {review._customer.__dict__}, Restaurant Data: {review._restaurant.__dict__}")
